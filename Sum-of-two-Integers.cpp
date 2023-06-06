7be87ea80fcd263353ef380f8a64041ec5f6c8cd
samratjagtap <100438026+samratjagtap@users.noreply.github.com>
2023-06-06 20:05:20 +0530

    Add files via upload
    Description
    
    You are given two integers A and B. Find their sum.
    
     
    
    
    Input Format
    
    The first line of the input contains one integer T - the number of test cases. Then T test cases follow.
    
    The first line of each test case contains two space-separated integers, A and B.
    
     
    
    
    Output Format
    
    For each test case, print the sum of A and B on a new line.
    
    
    Constraints
    
    1 ≤ T ≤ 2*105
    
    -109 ≤ A, B ≤ 109
    
     
    
    
    Sample Input 1
    
    4
    2 3
    -5 -10
    14215 142
    51 -141
     
    
    
    Sample Output 1
    
    5
    -15
    14357
    -90

---
+++ Sum-of-two-Integers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(){
+	int a,b;
+	cin>>a>>b;
+	int c = a+b;
+	cout<<c<<endl;
+
+
+}
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+
+	int _t;cin>>_t;while(_t--)
+	solve();
+}
